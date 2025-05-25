86b3bc8cee36f6ea39ee49fd236779c279574007
Musthafa <musthafa.jm@gmail.com>
2025-05-25 22:43:38 +0530

    first copy of Azure spring project

---
+++ src/main/java/com/sample/App.java
@@ -0,0 +1,24 @@
+package com.sample;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
+
+@RestController
+@RequestMapping("/api")
+class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Spring Boot App without DB!";
+    }
+
+}
